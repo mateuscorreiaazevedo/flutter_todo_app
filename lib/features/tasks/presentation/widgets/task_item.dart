c35c9e8d9f91a7b18649daaf400d057b38ee4f48
Mateus Azevedo <mateuscorreiaazevedo@gmail.com>
2026-05-20 14:42:05 -0300

    feat: add delete task with swipe dismissible

--- lib/features/tasks/presentation/widgets/task_item.dart
+++ lib/features/tasks/presentation/widgets/task_item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_app/features/tasks/domain/task.dart';
+import 'package:todo_app/features/tasks/domain/task_provider.dart';
 import 'package:todo_app/features/tasks/presentation/widgets/task_checkbox.dart';
 
 class TaskItem extends ConsumerWidget {
@@ -8,25 +9,39 @@ class TaskItem extends ConsumerWidget {
 
   const TaskItem({super.key, required this.task});
 
+  void _onDelete(WidgetRef ref) {
+    ref.read(taskProvider.notifier).deleteTask(task.id);
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return ListTile(
-      title: Text(
-        task.title,
-        style: TextStyle(
-          decoration: task.isDone ? TextDecoration.lineThrough : null,
-        ),
+    return Dismissible(
+      key: Key(task.id),
+      background: Container(
+        alignment: Alignment.centerRight,
+        color: Colors.red,
+        padding: EdgeInsets.only(right: 16),
+        child: Icon(Icons.delete, color: Colors.white),
       ),
-      subtitle: task.description == null
-          ? null
-          : Text(
-              task.description ?? '',
-              style: TextStyle(
-                fontStyle: task.isDone ? FontStyle.italic : null,
-                fontSize: 12,
+      onDismissed: (_) => _onDelete(ref),
+      child: ListTile(
+        title: Text(
+          task.title,
+          style: TextStyle(
+            decoration: task.isDone ? TextDecoration.lineThrough : null,
+          ),
+        ),
+        subtitle: task.description == null
+            ? null
+            : Text(
+                task.description ?? '',
+                style: TextStyle(
+                  fontStyle: task.isDone ? FontStyle.italic : null,
+                  fontSize: 12,
+                ),
               ),
-            ),
-      leading: TaskCheckbox(task: task),
+        leading: TaskCheckbox(task: task),
+      ),
     );
   }
 }
